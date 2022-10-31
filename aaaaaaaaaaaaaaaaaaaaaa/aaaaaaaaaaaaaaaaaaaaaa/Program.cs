1cf4031cf1261f54cba03e496e286720b250deb6
luizsiqueira17etec <112670742+luizsiqueira17etec@users.noreply.github.com>
2022-10-31 19:21:29 -0300

    aaa
    aaaa

---
+++ aaaaaaaaaaaaaaaaaaaaaa/aaaaaaaaaaaaaaaaaaaaaa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaaaaaaaaaaaaaaaaaaaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 20;
+            int num2 = 10;
+            int total;
+            total = num + num2;
+            Console.WriteLine("morra");
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
